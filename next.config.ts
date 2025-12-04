08bfc34c376b143bc5cb70f8da5a2496458b160c
Oleksii <maslak4it@gmail.com>
2025-12-04 11:20:48 +0200

    refactor(config): remove turbopack configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: "c:/Users/USER/Desktop/study/dice-game",
-  },
   reactStrictMode: false,
 };
 
